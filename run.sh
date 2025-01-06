c8df593db90d2c83c446606a429be2cf18c8f70d
elgeeko1 <11233838+elgeeko1@users.noreply.github.com>
2025-01-06 01:02:56 -0800

    Revise runscript with python3 binaries.

--- run.sh
+++ run.sh
@@ -13,15 +13,11 @@ set -e
 
 # install and activate python virtual environment
 if [ ! -f .venv/bin/activate ]; then
-    if ! which python3.10; then
-        echo "Error: python3.10 required"
-        exit 1
-    fi
     echo creating python virtual environment
-    python3.10 -m venv .venv
+    python3 -m venv .venv
     source .venv/bin/activate
     echo installing python requirements
-    pip install -r requirements.txt
+    pip3 install -r requirements.txt
 else
     source .venv/bin/activate
 fi
